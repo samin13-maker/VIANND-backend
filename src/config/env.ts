125a30e9a695ea625828580462d76ad85510085f
samin13-maker <243727@ids.upchiapas.edu.mx>
2026-03-06 15:35:15 -0600

    Eliminando todo el contenido

--- src/config/env.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const PORT = process.env.PORT || 3000;
-export const FDC_API_KEY = process.env.FDC_API_KEY || "";
--- src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {registerController,loginController} from "../controllers/foodController";
-
-const router = Router();
-
-router.post("/register", registerController);
-router.post("/login", loginController);
-
-export default router;
